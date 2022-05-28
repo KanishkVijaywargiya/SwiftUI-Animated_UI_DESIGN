cbbf35bb59aac66d00049a6326a7f23f050e73ac
KanishkVijaywargiya <secretcodersociety@gmail.com>
2022-05-28 19:27:06 +0530

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Kanishk Vijaywargiya on 28/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animated/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animated
+//
+//  Created by Kanishk Vijaywargiya on 28/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
